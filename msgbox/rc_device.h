6f6c03cf908bac534c87227d3b59c5c3bed02886
BPanther <bpanther_ts@hotmail.com>
2019-10-24 22:02:47 +0200

    event2 for h7

--- msgbox/rc_device.h
+++ msgbox/rc_device.h
@@ -13,6 +13,11 @@
 #elif HAVE_DUCKBOX_HARDWARE
 #define RC_DEVICE "/dev/input/event0"
 #define RC_DEVICE_FALLBACK "/dev/input/event1"
+
+#elif BOXMODEL_H7
+#define RC_DEVICE "/dev/input/event2"
+#define RC_DEVICE_FALLBACK "/dev/input/event1"
+
 #else
 #define RC_DEVICE "/dev/input/event1"
 #define RC_DEVICE_FALLBACK "/dev/input/event0"
