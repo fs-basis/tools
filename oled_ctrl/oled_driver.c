1358c75477a5ef46b1c6e83133bf1c49fe41d3b9
BPanther <bpanther_ts@hotmail.com>
2021-02-20 08:38:56 +0100

    oled_ctrl: fix for dm8000 original lcd

--- oled_ctrl/oled_driver.c
+++ oled_ctrl/oled_driver.c
@@ -27,6 +27,12 @@
 #include "lcd-ks0713.h"
 #include "oled_driver.h"
 
+#ifdef BOXMODEL_DM8000
+int DM8000 = 1;
+#else
+int DM8000 = 0;
+#endif
+
 int lcd_read_value(const char *filename)
 {
 	int value = 0;
@@ -39,7 +45,14 @@ int lcd_read_value(const char *filename)
 	}
 	else
 	{
-		value = -1;
+		if (DM8000 && filename == LCD_XRES)
+			value = 132;
+		else if (DM8000 && filename == LCD_YRES)
+			value = 64;
+		else if (DM8000 && filename == LCD_BPP)
+			value = 8;
+		else
+			value = -1;
 	}
 	return value;
 }
@@ -87,6 +100,12 @@ int lcd_open(const char *dev, int mode, int x_res, int y_res)
 		printf("%s: cannot read lcd bpp\n", __FUNCTION__);
 		return -1;
 	}
+
+	if (DM8000 && xres == 132 && yres == 64 && bpp == 8) {
+		printf("DM8000 original LCD not supported.\n", xres, yres, bpp);
+		return 0;
+	}
+
 	stride = xres * (bpp / 8);
 	return 0;
 }
@@ -288,9 +307,7 @@ void lcd_draw_character(FT_Bitmap* bitmap, FT_Int x, FT_Int y, int color)
 				continue;
 			}
 			if (bitmap->buffer[z] != 0x00) {
-				location = (j * (bpp / 8)) +
-					(i * stride);
-
+				location = (j * (bpp / 8)) + (i * stride);
 				if (bpp == 32) {
 					lcd_buffer[location] = RED(color);
 					lcd_buffer[location + 1] = GREEN(color);
