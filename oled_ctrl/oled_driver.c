ccee6074a68ce682fed510ded2364e6ceda77a3d
Frankenstone <dampf_acc@online.de>
2022-10-26 10:16:07 +0200

    Axas E4HD 4K Ultra fix oled part 2

--- oled_ctrl/oled_driver.c
+++ oled_ctrl/oled_driver.c
@@ -33,7 +33,11 @@ int lcd_read_value(const char *filename)
 	FILE *fd = fopen(filename, "r");
 	if (fd) {
 		int tmp;
+#if BOXMODEL_E4HDULTRA
+		if (fscanf(fd, "%d", &tmp) == 1)
+#else
 		if (fscanf(fd, "%x", &tmp) == 1)
+#endif
 			value = tmp;
 		fclose(fd);
 	}
