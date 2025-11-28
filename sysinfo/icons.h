d25e4bf6d85510a54a0ca8491630c32d5fcd9abf
Frankenstone <dampf_acc@online.de>
2025-11-28 10:51:39 +0100

    icons: fix divers, thx BP

--- sysinfo/icons.h
+++ sysinfo/icons.h
@@ -5,14 +5,15 @@
 #define ICONSDIR "/usr/share/tuxbox/neutrino/icons"
 #endif
 
-#define ICON_BUTTON_RED    ICONSDIR "/rot.png"
-#define ICON_BUTTON_GREEN  ICONSDIR "/gruen.png"
-#define ICON_BUTTON_YELLOW ICONSDIR "/gelb.png"
-#define ICON_BUTTON_BLUE   ICONSDIR "/blau.png"
+#define ICON_BUTTON_BLUE   ICONSDIR "/btn_blue.png"
+#define ICON_BUTTON_GREEN  ICONSDIR "/btn_green.png"
+#define ICON_BUTTON_RED    ICONSDIR "/btn_red.png"
+#define ICON_BUTTON_YELLOW ICONSDIR "/btn_yellow.png"
+
+#define ICON_BUTTON_DOWN   ICONSDIR "/down.png"
 #define ICON_BUTTON_HOME   ICONSDIR "/home.png"
 #define ICON_BUTTON_INFO   ICONSDIR "/info_small.png"
 #define ICON_BUTTON_OK     ICONSDIR "/ok.png"
-#define ICON_BUTTON_DOWN   ICONSDIR "/down.png"
 #define ICON_BUTTON_UP     ICONSDIR "/up.png"
 
 #endif
--- tuxwetter/icons.h
+++ tuxwetter/icons.h
@@ -2,9 +2,9 @@
 #define __icons_h__
 
 
-#define ICON_BUTTON_RED     "/share/tuxbox/neutrino/icons/rot.png"
-#define ICON_BUTTON_GREEN   "/share/tuxbox/neutrino/icons/gruen.png"
-#define ICON_BUTTON_YELLOW  "/share/tuxbox/neutrino/icons/gelb.png"
-#define ICON_BUTTON_BLUE    "/share/tuxbox/neutrino/icons/blau.png"
+#define ICON_BUTTON_BLUE    "/share/tuxbox/neutrino/icons/btn_blue.png"
+#define ICON_BUTTON_GREEN   "/share/tuxbox/neutrino/icons/btn_green.png"
+#define ICON_BUTTON_RED     "/share/tuxbox/neutrino/icons/btn_red.png"
+#define ICON_BUTTON_YELLOW  "/share/tuxbox/neutrino/icons/btn_yellow.png"
 
 #endif
